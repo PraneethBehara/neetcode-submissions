f8d06d9b32d6b8225644f4a68f2e3447371991e2
Praneeth Behara <praneeth.behara@gmail.com>
2026-07-03 14:10:29 -0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,36 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups: List[List[str]] = []
+        new_group = True
+
+        for each in strs:
+            new_group = True
+            # check if str is anagram of first str in the List        
+            for group in groups:
+                if self.is_anagram(each, group[0]):
+                    group.append(each)
+                    new_group = False
+                    break
+            
+            if new_group:
+                groups.append([each])
+            
+        print(groups)
+        return groups
+
+    def is_anagram(self, s, t):
+        if len(s) != len(t):
+            return False
+
+        countS = self.get_str_count(s)
+        countT = self.get_str_count(t)
+
+        return countS == countT
+
+
+    def get_str_count(self, s: str) -> dict[str, int]:
+        countS = {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+
+        return countS
